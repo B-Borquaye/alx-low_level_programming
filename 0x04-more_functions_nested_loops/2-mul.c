ef23a3e9694400b8158122f69cf0465ebf0cf9da
Barnabas Borquaye <barnabasborquaye@gmail.com>
2023-08-17 18:39:38 -0700

    Mandatory task

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * 
+ * Return: multiplication of a and b.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * 
+ * Return: no return.
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(95);
+	}
+	_putchar('\n');
+}
